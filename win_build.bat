2fa22b768403e24b6060f7b3770a100b58a279ed
nialredha <nialredha@gmail.com>
2024-12-29 14:00:44 -0600

    Update README and build script

--- win_build.bat
+++ win_build.bat
@@ -1,14 +1,14 @@
 @echo off
 
-set path=C:\SDL2\lib\x64;%path%
+set path=C:\Software\SDL2\lib\x64;%path%
 set SDL_AUDIODRIVER=dsound
 
 if not exist "build\" mkdir build\
 cd build/
 
 set FLAGS=/EHsc
-set INCLUDES=/I ..\src /I C:\SDL2\include 
-set SDL_PATH=C:\SDL2\lib\x64\
+set INCLUDES=/I ..\src /I C:\Software\SDL2\include 
+set SDL_PATH=C:\Software\SDL2\lib\x64\
 set LIBS=user32.lib shell32.lib SDL2.lib SDL2main.lib SDL2_ttf.lib 
 
 cl %FLAGS% %INCLUDES% ..\src\main.cpp ..\src\audio.cpp ..\src\graphics.cpp /link /LIBPATH:%SDL_PATH% %LIBS% /SUBSYSTEM:CONSOLE
